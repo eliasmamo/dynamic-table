8d121f7600be258d5778bbead45674cf01feba9b
Elias <elias.mamo@gmail.com>
2018-04-28 17:25:55 +0300

    Remove router as we dont need it for this simple app

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@
 import Vue from 'vue';
 import InputTag from 'vue-input-tag';
 import App from './App';
-import router from './router';
 import store from './store/store';
 
 Vue.config.productionTip = false;
@@ -12,7 +11,6 @@ Vue.component('InputTag', InputTag);
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
   store,
   components: { App },
   template: '<App/>',
